98af9dd650941a5b805a355bfa8c45ce044cfd5c
Utsob Roy <roy@utsob.me>
2022-12-08 17:03:13 +0600

    better benchmark output

--- README.md
+++ README.md
@@ -6,20 +6,19 @@ Every day has a dedicated file in the `solutions` directory, and two inputs (one
 
 Shamelessly stole and adapted [the scaffolding mechanism found here](https://github.com/fspoettel/advent-of-code-rust) to my need.
 
-
 ## Benchmarks
 
-|              | Part 01                 | Part 02                         |
-|:-------------|:--------------------------|:--------------------------------- |
-| **Day 01** | 42.56 μs   | 42.49 μs    |
-| **Day 02** | 177.97 μs  | 173.17 μs   |
-| **Day 03** | 185.94 μs  | 260.57 μs     |
-| **Day 04** | 80.21 μs   | 74.20 μs    |
-| **Day 05** | 85.44 μs   | 85.59 μs    |
-| **Day 06** | 16.14 μs   | 29.02 μs      |
-| **Day 07** | 145.29 μs  | 149.35 μs   |
-| **Day 08** | 411.52 μs  | 369.71 μs   |
+| Day/Part | Part 1 | Part 2 | Total |
+|:---------|-------:|-------:|------:|
+| **Day 01** | 45.04μs | 45.10μs | 90.15μs |
+| **Day 02** | 180.52μs | 172.95μs | 353.47μs |
+| **Day 03** | 188.21μs | 264.21μs | 452.42μs |
+| **Day 04** | 74.30μs | 74.23μs | 148.54μs |
+| **Day 05** | 85.31μs | 85.36μs | 170.67μs |
+| **Day 06** | 16.14μs | 29.35μs | 45.49μs |
+| **Day 07** | 144.37μs | 148.76μs | 293.13μs |
+| **Day 08** | 424.44μs | 376.52μs | 800.97μs |
 
----
-Made with [criterion-table](https://github.com/nu11ptr/criterion-table)
+
+**Total runtime: 2.35ms**
 
---
+++ src/bin/format_bench.rs
@@ -0,0 +1,199 @@
+use std::{
+    collections::HashMap,
+    io::{self, BufRead, StdoutLock, Write},
+};
+
+const TOP_TEMPLATE: &str = r###"## Benchmarks
+
+| Day/Part | Part 1 | Part 2 | Total |
+|:---------|-------:|-------:|------:|
+"###;
+
+#[derive(Debug, Clone)]
+struct BenchData {
+    id: String,
+    value: f64,
+    unit: String,
+}
+
+#[derive(Debug, Clone)]
+struct Value {
+    value: f64,
+    unit: String,
+}
+
+#[derive(Debug, Clone)]
+struct Row {
+    day: String,
+    part1: Option<Value>,
+    part2: Option<Value>,
+}
+
+const UNITS: &[&str] = &["s", "ms", "us", "ns", "ps"];
+
+fn add_durations(v1: &Value, v2: &Value) -> Value {
+    if v1.unit == v2.unit {
+        Value {
+            value: v1.value + v2.value,
+            unit: v1.unit.clone(),
+        }
+    } else {
+        let v1_unit_pos = UNITS.iter().position(|r| r == &v1.unit.clone()).unwrap();
+        let v2_unit_pos = UNITS.iter().position(|r| r == &v2.unit.clone()).unwrap();
+        if v1_unit_pos > v2_unit_pos {
+            Value {
+                value: (v1.value / (1000.0_f64.powf((v1_unit_pos - v2_unit_pos) as f64)))
+                    + v2.value,
+                unit: v2.unit.clone(),
+            }
+        } else {
+            Value {
+                value: (v2.value / (1000.0_f64.powf((v2_unit_pos - v1_unit_pos) as f64)))
+                    + v1.value,
+                unit: v1.unit.clone(),
+            }
+        }
+    }
+}
+
+fn format_time(v: &Value) -> String {
+    let (mut unit, mut value) = (v.unit.clone(), v.value);
+    while value > 1000.0 {
+        value /= 1000.0;
+        unit = match unit.as_str() {
+            "ms" => "s".to_string(),
+            "us" => "ms".to_string(),
+            "ns" => "us".to_string(),
+            "ps" => "ns".to_string(),
+            _ => "".to_string(),
+        }
+    }
+    if unit == *"us" {
+        unit = "μs".to_string()
+    }
+
+    format!("{:.2}{}", value, unit)
+}
+
+fn format_row(r: &Row, writer: &mut StdoutLock) -> Value {
+    let mut times = Vec::new();
+    let part_1 = match &r.part1 {
+        Some(v) => {
+            times.push(v);
+            format_time(v)
+        }
+        None => "".to_string(),
+    };
+
+    let part_2 = match &r.part2 {
+        Some(v) => {
+            times.push(v);
+            format_time(v)
+        }
+        None => "".to_string(),
+    };
+    let total = if times.len() < 2 {
+        times[0].clone()
+    } else {
+        add_durations(times[0], times[1])
+    };
+    writer
+        .write_all(
+            format!(
+                "| **{}** | {} | {} | {} |\n",
+                r.day,
+                part_1,
+                part_2,
+                format_time(&total)
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    total
+}
+
+impl BenchData {
+    fn from_string(line: &str) -> Option<Self> {
+        let json: serde_json::Value = serde_json::from_str(line).unwrap();
+        if json.get("reason").unwrap() == "benchmark-complete" {
+            return Some(BenchData {
+                id: json["id"].as_str().unwrap().to_string(),
+                value: json["typical"]["estimate"].as_f64().unwrap(),
+                unit: json["typical"]["unit"].as_str().unwrap().to_string(),
+            });
+        }
+
+        None
+    }
+}
+
+fn main() {
+    let lines = io::stdin().lock().lines();
+    let mut rows = HashMap::new();
+    for line in lines {
+        if let Ok(line_content) = line {
+            let b_data = BenchData::from_string(&line_content);
+            if let Some(row_data) = b_data {
+                let mut parts = row_data.id.split('/').skip(1);
+                let part = parts.next().unwrap().to_owned();
+                let day = parts.next().unwrap().to_owned();
+                let row = rows.entry(day.clone()).or_insert_with(|| {
+                    if part == "Part 01" {
+                        Row {
+                            day: day.clone(),
+                            part1: Some(Value {
+                                value: row_data.value,
+                                unit: row_data.unit.clone(),
+                            }),
+                            part2: None,
+                        }
+                    } else {
+                        Row {
+                            day: day.to_string(),
+                            part2: Some(Value {
+                                value: row_data.value,
+                                unit: row_data.unit.clone(),
+                            }),
+                            part1: None,
+                        }
+                    }
+                });
+                // println!("{:?}", row);
+                if part == "Part 01" {
+                    row.part1 = Some(Value {
+                        value: row_data.value,
+                        unit: row_data.unit.clone(),
+                    });
+                } else {
+                    row.part2 = Some(Value {
+                        value: row_data.value,
+                        unit: row_data.unit.clone(),
+                    });
+                }
+            }
+        } else {
+            break;
+        }
+    }
+    let stdout = io::stdout();
+    let mut writer = stdout.lock();
+    writer.write_all(TOP_TEMPLATE.as_bytes()).unwrap();
+    let mut values: Vec<&Row> = rows.values().collect();
+    values.sort_by(|a, b| a.day.cmp(&b.day));
+    let mut total = None;
+    for v in values {
+        let v_total = format_row(v, &mut writer);
+        if total.is_none() {
+            total = Some(v_total.clone());
+        } else {
+            total = Some(add_durations(&total.unwrap(), &v_total));
+        }
+    }
+    writer.write_all("\n\n".as_bytes()).unwrap();
+    if let Some(t) = total {
+        writer
+            .write_all(format!("**Total runtime: {}**", format_time(&t)).as_bytes())
+            .unwrap();
+        writer.write_all("\n\n".as_bytes()).unwrap();
+    }
+}
