f3bc5faf12e6335839adc954b8f1fd337002dcb9
Utsob Roy <roy@utsob.me>
2022-12-08 09:59:00 +0600

    refactored day 7

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -9,8 +9,11 @@ pub struct Directory {
 }
 
 impl Directory {
-    fn new_with_parent(p: Option<String>) -> Self {
-        Self { parent: p, size: 0 }
+    fn new_with_parent(p: &Option<String>) -> Self {
+        Self {
+            parent: p.clone(),
+            size: 0,
+        }
     }
 }
 
@@ -20,13 +23,11 @@ fn update_size(
     size_cache: usize,
 ) -> usize {
     if size_cache > 0 {
-        let d = dir_map.get_mut(current_dir.as_ref().unwrap()).unwrap();
-        d.size += size_cache;
-        let mut dp = d.parent.clone();
-        while let Some(dp_key) = dp {
-            let dpt = dir_map.get_mut(&dp_key).unwrap();
-            dpt.size += size_cache;
-            dp = dpt.parent.clone();
+        let mut current_dir = current_dir.clone();
+        while let Some(dir_key) = current_dir {
+            let dir = dir_map.get_mut(&dir_key).unwrap();
+            dir.size += size_cache;
+            current_dir = dir.parent.clone();
         }
     }
     0
@@ -35,48 +36,34 @@ fn update_size(
 fn run_commands(input: String) -> HashMap<String, Directory> {
     let mut current_dir: Option<String> = None;
     let mut dir_map: HashMap<String, Directory> = HashMap::new();
-    dir_map.insert("/".to_string(), Directory::new_with_parent(None));
+    dir_map.insert("/".to_string(), Directory::new_with_parent(&None));
     let mut size_cache = 0;
     for line in input.lines() {
-        if line.starts_with('$') {
-            let mut parts = line.split(' ').skip(1);
-            if parts.next().unwrap() == "cd" {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        match parts.as_slice() {
+            ["$", "cd", ".."] => {
                 size_cache = update_size(&mut dir_map, &current_dir, size_cache);
-                let pwd = parts.next().unwrap();
-                match pwd {
-                    ".." => {
-                        let mut new_current_dir = None;
-                        if current_dir.is_some() {
-                            let cur = current_dir.clone().unwrap();
-                            new_current_dir = dir_map.get(&cur).unwrap().parent.clone();
-                        }
-                        current_dir = new_current_dir;
-                    }
-                    _ => {
-                        let mut key = pwd.to_string();
-                        if current_dir.is_some() {
-                            key = format!("{}{}/", current_dir.as_ref().unwrap(), pwd);
-                        }
-                        current_dir = Some(key);
-                    }
+                if current_dir.is_some() {
+                    current_dir = dir_map.get(&current_dir.unwrap()).unwrap().parent.clone();
                 }
             }
-        } else {
-            let mut parts = line.split(' ');
-            match parts.next().unwrap() {
-                "dir" => {
-                    let dir = parts.next().unwrap();
-                    let key = format!("{}{}/", current_dir.as_ref().unwrap(), dir);
-                    dir_map
-                        .entry(key)
-                        .or_insert_with(|| Directory::new_with_parent(current_dir.clone()));
-                }
-                size => {
-                    let s = size.parse::<usize>().unwrap();
-                    size_cache += s;
-                }
+            ["$", "cd", pwd] => {
+                size_cache = update_size(&mut dir_map, &current_dir, size_cache);
+                current_dir = Some(match current_dir {
+                    Some(cur_dir) => format!("{}{}/", cur_dir, pwd),
+                    None => pwd.to_string(),
+                });
             }
-        }
+            ["dir", dir] => {
+                let key = format!("{}{}/", current_dir.as_ref().unwrap(), dir);
+                dir_map
+                    .entry(key)
+                    .or_insert_with(|| Directory::new_with_parent(&current_dir));
+            }
+            ["$", "ls"] => continue,
+            [size, _] => size_cache += size.parse::<usize>().unwrap(),
+            _ => unreachable!(),
+        };
     }
     update_size(&mut dir_map, &current_dir, size_cache);
     dir_map
@@ -88,9 +75,8 @@ fn get_all_dir_sizes(dir_map: &HashMap<String, Directory>) -> Vec<usize> {
 
 pub fn solution_day_07_01(file_path: String) -> Option<usize> {
     let input = fs::read_to_string(file_path).unwrap().trim().to_string();
-    let dir_map = run_commands(input);
     Some(
-        get_all_dir_sizes(&dir_map)
+        get_all_dir_sizes(&run_commands(input))
             .iter()
             .filter(|s| s <= &&100000)
             .sum(),
@@ -99,8 +85,7 @@ pub fn solution_day_07_01(file_path: String) -> Option<usize> {
 
 pub fn solution_day_07_02(file_path: String) -> Option<usize> {
     let input = fs::read_to_string(file_path).unwrap().trim().to_string();
-    let dir_map = run_commands(input);
-    let mut sizes = get_all_dir_sizes(&dir_map);
+    let mut sizes = get_all_dir_sizes(&run_commands(input));
     sizes.sort();
     let (total_size, required_space, used) = (70000000, 30000000, sizes.last().unwrap());
     let to_be_reclaimed = required_space - (total_size - used);
