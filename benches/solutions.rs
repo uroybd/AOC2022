74a5b9c73508f60fadbbcb683b11c0e3d5bd7089
Utsob Roy <roy@utsob.me>
2022-12-10 12:07:52 +0600

    day 10 complete

--- benches/solutions.rs
+++ benches/solutions.rs
@@ -7,6 +7,7 @@ use aoc_2022::solutions::day06::{solution_day_06_01, solution_day_06_02};
 use aoc_2022::solutions::day07::{solution_day_07_01, solution_day_07_02};
 use aoc_2022::solutions::day08::{solution_day_08_01, solution_day_08_02};
 use aoc_2022::solutions::day09::{solution_day_09_01, solution_day_09_02};
+use aoc_2022::solutions::day10::{solution_day_10_01, solution_day_10_02};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn all_solutions(c: &mut Criterion) {
@@ -91,6 +92,15 @@ fn all_solutions(c: &mut Criterion) {
     });
     g.bench_function("Part 02/Day 09", |b| {
         b.iter(|| solution_day_09_02(black_box(file_path.clone())))
+    });
+
+    // Day 10
+    let file_path: String = String::from("src/inputs/day09.txt");
+    g.bench_function("Part 01/Day 10", |b| {
+        b.iter(|| solution_day_10_01(black_box(file_path.clone())))
+    });
+    g.bench_function("Part 02/Day 10", |b| {
+        b.iter(|| solution_day_10_02(black_box(file_path.clone())))
     });
 }
 
---
+++ src/solutions/day10.rs
@@ -0,0 +1,108 @@
+// Advent of Code 2022 - Day 10
+
+use crate::utils::read::read_lines;
+
+pub struct ClockCircuit {
+    cycle: usize,
+    value: isize,
+    strength: isize,
+    image: Vec<String>,
+}
+
+impl ClockCircuit {
+    fn new() -> Self {
+        Self {
+            cycle: 0,
+            value: 1,
+            strength: 0,
+            image: vec![".".to_string(); 240],
+        }
+    }
+
+    fn print_image(&self) {
+        self.image.chunks(40).for_each(|line| {
+            println!("{}", line.join(""));
+        });
+    }
+
+    fn cycle_count(&mut self) {
+        if (self.value - ((self.cycle % 40) as isize)).abs() == 1 {
+            self.image[self.cycle] = "#".to_string();
+        }
+
+        self.cycle += 1;
+        if self.cycle % 40 == 20 {
+            self.strength += (self.cycle as isize) * self.value;
+        }
+    }
+
+    fn execute(&mut self, instruction: &str) {
+        if instruction == "noop" {
+            self.cycle_count();
+        } else {
+            self.cycle_count();
+            self.cycle_count();
+            let amount: isize = instruction
+                .split_whitespace()
+                .nth(1)
+                .unwrap()
+                .parse()
+                .unwrap();
+            self.value += amount;
+        }
+    }
+}
+
+pub fn solution_day_10_01(file_path: String) -> Option<isize> {
+    let mut circuit = ClockCircuit::new();
+    read_lines(file_path)
+        .iter()
+        .for_each(|ins| circuit.execute(ins));
+    Some(circuit.strength)
+}
+
+pub fn solution_day_10_02(file_path: String) -> Option<usize> {
+    let mut circuit = ClockCircuit::new();
+    read_lines(file_path)
+        .iter()
+        .for_each(|ins| circuit.execute(ins));
+    circuit.print_image();
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day_10_01() {
+        let file_path: String = String::from("src/inputs/day10e.txt");
+        let result = solution_day_10_01(file_path).unwrap();
+        assert_eq!(result, 13140);
+    }
+
+    #[test]
+    fn test_day_10_02() {
+        let file_path: String = String::from("src/inputs/day10e.txt");
+        let result = solution_day_10_02(file_path);
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    #[ignore]
+    fn output_day_10_01() {
+        let file_path: String = String::from("src/inputs/day10.txt");
+        let result = solution_day_10_01(file_path);
+        dbg!(result.unwrap());
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    #[ignore]
+    fn output_day_10_02() {
+        let file_path: String = String::from("src/inputs/day10.txt");
+        let result = solution_day_10_02(file_path);
+        // dbg!(result.unwrap());
+        assert_eq!(result, None);
+    }
+}
