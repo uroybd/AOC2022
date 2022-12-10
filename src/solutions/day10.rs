92d4bec155bf4ff7627793dc07002c232bcb15ca
Utsob Roy <roy@utsob.me>
2022-12-10 14:25:23 +0600

    updates

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -93,7 +93,6 @@ mod tests {
     fn output_day_10_01() {
         let file_path: String = String::from("src/inputs/day10.txt");
         let result = solution_day_10_01(file_path);
-        dbg!(result.unwrap());
         assert_eq!(1, 1);
     }
 
@@ -102,7 +101,6 @@ mod tests {
     fn output_day_10_02() {
         let file_path: String = String::from("src/inputs/day10.txt");
         let result = solution_day_10_02(file_path);
-        // dbg!(result.unwrap());
         assert_eq!(result, None);
     }
 }
